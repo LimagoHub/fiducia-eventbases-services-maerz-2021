1c3438c74a992da925ac61b8d1c1123a81396c4c
limagoHub <limagoHub@limago.de>
2021-03-09 09:51:01 +0100

    Initial commit

---
+++ kreditantragservice/src/main/java/de/fiducia/kreditantragservice/KreditAntragServiceApplication.java
@@ -0,0 +1,13 @@
+package de.fiducia.kreditantragservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KreditAntragServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KreditAntragServiceApplication.class, args);
+	}
+
+}

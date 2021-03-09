4ee47d57e6fd00a444e62208600a4a29ad49f4e4
limagoHub <limagoHub@limago.de>
2021-03-09 08:30:42 +0100

    Channels

--- kafkaproducer/src/main/java/de/fiducia/kafkaproducer/event/messaging/MyChannels.java
+++ kafkaproducer/src/main/java/de/fiducia/kafkaproducer/event/messaging/MyChannels.java
@@ -1,13 +1,20 @@
 package de.fiducia.kafkaproducer.event.messaging;
 
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 public interface MyChannels {
 	
 	String PERSON_ERASST = "personErfasstOut";
+	String SCORING_POSITIVE = "scoringRequestIn";
+	
 	
 	@Output
 	MessageChannel personErfasstOut();
+	
+	@Input
+	SubscribableChannel scoringRequestIn();
 
 }
